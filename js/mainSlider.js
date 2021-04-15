c84aad5f1fea010b24418a129f8e79b6b9f69747
Maksym_Davydenko <maksim.davydenko.work@gmail.com>
2021-04-15 22:45:26 +0300

    fix: slider nav buttons

--- js/mainSlider.js
+++ js/mainSlider.js
@@ -1,16 +1,24 @@
 $( document ).ready(function() {
 
-$(".js-main-slider").slick({
+  $(".js-main-slider").slick({
+      slidesToShow: 1,
+      infinite: true,
+      autoplay: true,
+      dots: true,
+      arrows: false,
+  });
 
-    // normal options...
-    slidesToShow: 1,
-    // the magic
-    infinite: false,
-  autoplay: true,
-  dots: true,
-  arrows: false
-});
+  $( ".slick-dots" ).wrap('<div class="main-slider__nav"></div>')
+  $( ".main-slider__nav" ).prepend( '<div class="controls-btn upBtn"><img src="./img/icons/arrowUpBlue.svg" alt="up"></div>' );
+  $( ".main-slider__nav" ).append( '<div class="controls-btn downBtn"><img src="./img/icons/arrowDownBlue.svg" alt="up"></div>');
 
-$(".slick-dots").wrap("<div class='container'><div class='main-slider__nav-wrap'><div class='main-slider__nav'><div id='next-btn'></div> <div id='prev'></div></div></div>")
+  $('.upBtn').on('click', function () {
+    $('.js-main-slider').slick('slickPrev');
+  });
 
+  $('.downBtn').on('click', function () {
+    $('.js-main-slider').slick('slickNext');
+
+  });
+  $( ".main-slider__inner" ).append($('.main-slider__nav'));
 })
