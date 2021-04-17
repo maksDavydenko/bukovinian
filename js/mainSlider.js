bf6082bde5c5523b36001fcb3073d456668823a4
Maksym_Davydenko <maksim.davydenko.work@gmail.com>
2021-04-17 13:45:46 +0300

    add: new news.html file

--- js/mainSlider.js
+++ js/mainSlider.js
@@ -3,10 +3,12 @@ $( document ).ready(function() {
   $(".js-main-slider").slick({
       slidesToShow: 1,
       infinite: true,
-      autoplay: true,
+      // autoplay: true,
       dots: true,
       arrows: false,
   });
+
+  const linksArr = ["http://www.google.com/", "https://www.youtube.com/", "https://www.instagram.com/", "https://www.instagram.com/", "https://twitter.com/?lang=ru" ]
 
   $( ".slick-dots" ).wrap('<div class="main-slider__nav"></div>')
   $( ".main-slider__nav" ).prepend( '<div class="controls-btn upBtn"><img src="./img/icons/arrowUpBlue.svg" alt="up"></div>' );
@@ -18,7 +20,11 @@ $( document ).ready(function() {
 
   $('.downBtn').on('click', function () {
     $('.js-main-slider').slick('slickNext');
-
   });
+
   $( ".main-slider__inner" ).append($('.main-slider__nav'));
+
+  $('.js-main-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.js-slider-link').attr("href", linksArr[currentSlide])
+});
 })
