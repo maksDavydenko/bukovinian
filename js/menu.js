20bb2846fab02ea55a7a58f98898b862761c3869
Maksym_Davydenko <maksim.davydenko.work@gmail.com>
2021-04-25 17:26:34 +0300

    add: search mobile

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,19 @@
 const menuBtn = document.getElementById('menu');
 const menuWrap = document.querySelector('.menu-nav__list');
+const searchMobile = document.querySelector('.search-mobile');
+const searchMobileInput = document.querySelector('.js-search-input');
 
 menuBtn.addEventListener('click', () => {
     menuWrap.style.display === 'flex' ?  
     menuWrap.style.display = 'none' :  
     menuWrap.style.display = 'flex'
 });
+
+searchMobile.addEventListener('click', e => {
+    !e.target.closest('Input') ?
+     searchMobileInput.classList.toggle('display-block'): 
+     null;
+     
+     searchMobileInput.focus()
+     
+})
